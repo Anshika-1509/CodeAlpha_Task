98fe6d9967aafaa5dfe82f40324c96b60e72ec43
Anshika Shukla <anitashukla4655@gmail.com>
2025-07-14 12:45:47 +0530

    Delete portfolio directory

--- portfolio/script.js
+++
@@ -1,8 +0,0 @@
-const hamburger=document.getElementById('hamburger');
-const list=document.getElementById('list');
-const overlay=document.getElementById('overlay');
-
-hamburger.addEventListener('click',()=>{
-    list.classList.toggle("navlist-active");
-    overlay.classList.toggle("overlay-active");
-});
